c3c8d8f7d1772d45609fa197f8575205a27a44c7
John <jaboyak@gmail.com>
2019-08-23 12:35:52 -0700

    first commit

---
+++ src/test/java/com/galvabank/portalapi/PortalApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.galvabank.portalapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PortalApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
